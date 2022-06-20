63f916161169e19726930aac3e05ad911daccc43
gbianchi <topcat_arg@yahoo.com>
2022-06-20 20:42:51 -0300

    otro test fallido

--- Controllers/SecurityController.cs
+++ Controllers/SecurityController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Collections.Immutable;
 using Prode2022Server.Models;
 using Prode2022Server.Helpers;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
 
 namespace Prode2022Server.Controllers
 {
@@ -13,11 +16,15 @@ namespace Prode2022Server.Controllers
     {
 
         SecurityServices securityServices;
+        IHttpContextAccessor HttpContextAccessor;
 
-        public SecurityController(SecurityServices ses)
+        public SecurityController(SecurityServices ses,
+            IHttpContextAccessor httpContextAccessor)
         {
             securityServices = ses;
+            HttpContextAccessor = httpContextAccessor;
         }
+
         [Route("CreateUser")]
         [HttpPost]
         public async Task<bool> CreateUser(NewUserData user)
@@ -43,7 +50,23 @@ namespace Prode2022Server.Controllers
                 user.LoggedIn = false;
                 return user;
             }
-            return await securityServices.CheckUser(user);
+            user = await securityServices.CheckUser(user);
+            if (user.LoggedIn)
+            {
+                //create a session for the user.
+                var claims = new List<Claim>();
+                claims.Add(new (ClaimTypes.Name,user.Name!));
+                claims.Add(new (ClaimTypes.Email,user.Email!));
+                var claimsIdentity = new ClaimsIdentity(
+                    claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var v = await HttpContextAccessor.HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity)
+                );
+                
+            }
+            bool isok = HttpContextAccessor.HttpContext.User.Identity.IsAuthenticated;
+            return user;
         }
     }
 }
