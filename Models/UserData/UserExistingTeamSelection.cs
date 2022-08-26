15db4de8cb5b5ee258829e812e63c2d4456eb7de
gbianchi <topcat_arg@yahoo.com>
2022-08-26 17:39:23 -0300

    yy

---
+++ Models/UserData/UserExistingTeamSelection.cs
@@ -0,0 +1,14 @@
+﻿namespace Prode2022Server.Models.UserData;
+
+using System.Collections.Immutable;
+using System.ComponentModel.DataAnnotations;
+
+public class UserExistingTeamSelection
+{
+    public int SelectedTeam { get; set; } = 0;
+    public ImmutableArray<UserTournament> TeamList { get; set; } = new();
+    public string TournamentPassword { get; set; } = "";
+    [Compare("TournamentPassword", ErrorMessage = "La contraseña no es correcta")]
+    public string Password { get; set; } = "";
+}
+
