4380bcde72ca40ebaffde1137187ebec34c52a47
Gonzalo <topcat_arg@yahoo.com>
2022-09-13 14:14:21 -0300

    Changed forecast page for mobile. Also allow to save all

--- Models/UserForecast/Match.cs
+++ Models/UserForecast/Match.cs
@@ -8,9 +8,40 @@ public class Match
     public string Team1Flag { get; set; } = "";
     public string Team2Flag { get; set; } = "";
 
-    public int Team1Goals { get; set; }
-    public int Team2Goals { get; set; }
+    private int team1Goals;
+    public int Team1Goals 
+    { 
+        get
+        {
+            return team1Goals;
+        }
+        set
+        {
+            team1Goals = value;
+            checkGoals();
+        }
+    }
 
+    public int OriginalTeam1Goals { get; set; }
+    private int team2Goals;
+    public int Team2Goals 
+    { 
+        get
+        {
+            return team2Goals;
+        }
+        set
+        {
+            team2Goals = value;
+            checkGoals();
+        }
+    }
+    public int OriginalTeam2Goals { get; set; }
+
+    private void checkGoals()
+    {
+        Modified = (team1Goals != OriginalTeam1Goals) || (team2Goals != OriginalTeam2Goals);
+    }
     public bool Closed { get; set; }
 
     public string MyTeamName { get; set; } = "";
@@ -19,5 +50,6 @@ public class Match
 
     public string StageName { get; set; } = "";
     public string Date { get; set; } = "";
+    public bool Modified { get; set; } = false;
 
 }
